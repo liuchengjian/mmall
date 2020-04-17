b110a04f7ad3d82570f671ee5f14de2e0321c9fd
LENOVO <627107345@qq.com>
2020-04-17 20:07:21 +0800

    qq

--- src/main/java/liucj/controller/backend/ProductManageController.java
+++ src/main/java/liucj/controller/backend/ProductManageController.java
@@ -1,21 +1,26 @@
 package liucj.controller.backend;
 
+import com.google.common.collect.Maps;
 import liucj.common.Const;
 import liucj.common.ResponseCode;
 import liucj.common.ServerResponse;
 import liucj.pojo.Product;
 import liucj.pojo.User;
+import liucj.service.IFileService;
 import liucj.service.IProductService;
 import liucj.service.IUserService;
+import liucj.utils.PropertiesUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.Date;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/manage/product")
@@ -24,7 +29,8 @@ public class ProductManageController {
     private IProductService iProductService;
     @Autowired
     private IUserService iUserService;
-
+    @Autowired
+    private IFileService iFileService;
     /**
      * 商品保存
      * @param session
@@ -68,4 +74,26 @@ public class ProductManageController {
             return ServerResponse.createByErrorMessage("无权限操作");
         }
     }
+
+    @RequestMapping("upload.do")
+    @ResponseBody
+    public ServerResponse upload(HttpSession session, @RequestParam(value = "upload_file",required = false) MultipartFile file, HttpServletRequest request){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+        }
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            String path = request.getSession().getServletContext().getRealPath("upload");
+            String targetFileName = iFileService.upload(file,path);
+            String url = PropertiesUtil.getProperty("ftp.server.http.prefix")+targetFileName;
+
+            Map fileMap = Maps.newHashMap();
+            fileMap.put("uri",targetFileName);
+            fileMap.put("url",url);
+            return ServerResponse.createBySuccess(fileMap);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
 }
