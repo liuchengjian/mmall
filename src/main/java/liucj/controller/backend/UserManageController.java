8e832059d378760ff4cf1feb6f3d16794014bb3e
lchj <627107345@qq.com>
2020-07-08 17:40:28 +0800

    新增用户列表接口

---
+++ src/main/java/liucj/controller/backend/UserManageController.java
@@ -0,0 +1,55 @@
+package liucj.controller.backend;
+
+import liucj.common.Const;
+import liucj.common.ServerResponse;
+import liucj.pojo.User;
+import liucj.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by geely
+ */
+
+@Controller
+@RequestMapping("/manage/user")
+public class UserManageController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    @RequestMapping(value = "login.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession session) {
+        ServerResponse<User> response = iUserService.login(username, password);
+        if (response.isSuccess()) {
+            User user = response.getData();
+            if (user.getRole() == Const.Role.ROLE_ADMIN) {
+                //说明登录的是管理员
+                session.setAttribute(Const.CURRENT_USER, user);
+                return response;
+            } else {
+                return ServerResponse.createByErrorMessage("不是管理员,无法登录");
+            }
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "list.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> list(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                     @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+        ServerResponse<User> response = iUserService.list(pageNum, pageSize);
+        if (response.isSuccess()) {
+            return ServerResponse.createBySuccess(response.getData());
+        }
+        return ServerResponse.createByErrorMessage("获取用户数据错误");
+    }
+
+}
